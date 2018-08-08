f3a436b992ae14abf4ce1b3935478dd0f39eea06
daniel <seahagos@gmail.com>
2018-08-08 17:02:52 -0500

    answered higher order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,12 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+function isEven(value, index, array){
+  if(value % 2 === 0){
+   return value;
+  }
+}
+let evenNumbers = mixedNumbers.filter(isEven); // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -44,8 +49,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
+let postTaxPrices =prices.map((val, index, wholeArray) => {
+  // return element to new Array
+  return (val*.07)+wholeArray;
+});
 
 
 ////////// PROBLEM 3 //////////
@@ -63,7 +71,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+
+let totalPopulation=populations.reduce((acc, val) => {
+  return acc + val;
+});
 
 
 
@@ -89,7 +100,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest=monstersInYourPocket.filter(val => {
+  return val.CP >200;
+}); 
 
 
 
@@ -106,8 +119,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get the sum of all the order totals after adding in the sales tax
 */
 
-let ordersTotal //Code Here
-
+let ordersTotal =orders.map((acc, val) => {
+  return acc.price + val.tax;
+});
 
 
 ////////// PROBLEM 6 //////////
@@ -126,6 +140,7 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
-
+let bobsTotal = purchases.reduce((accumulator, currentValue, index, array) => {
+  return currentValue.owner === 'Bob' ? accumulator + currentValue.price : accumulator
+},0);
+console.log(bobsTotal)
